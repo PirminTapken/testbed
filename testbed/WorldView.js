a5cbbe5dc6e777b5298ad2420093411dda281a42
Pirmin Fix <pirmin.fix@desy.de>
2012-01-23 14:53:59 +0100

    added stuff for debugging

--- testbed/WorldView.js
+++ testbed/WorldView.js
@@ -2,6 +2,7 @@ goog.provide('testbed.WorldView');
 
 goog.require('lime.Scene');
 goog.require('lime.Layer');
+goog.require('lime.Label');
 
 /**
  * @constructor
@@ -12,11 +13,24 @@ testbed.WorldView = function(world)
 {
 	goog.base(this);
 
+	var size = new Array(1024,768);
+
+	this.appendChild(
+		new lime.Label().
+			setText("Hello World!").
+			setFontSize(30).
+			setFontFamily('fantasy').
+			setPosition(size[0] / 2, size[1] / 2));
+
 	/** @private */
-	this.groundlayer_ = new lime.Layer();
+	this.groundlayer_ = new lime.Layer().
+		setSize(size[0], size[1]);
 	for (i = 0; i < world.length; i++) {
+		this.groundlayer_.appendChild(new lime.Label().setText("grass!").setPosition(size[0] / world.length * i, 0));
+		world[0][i].setPosition(size[0] / world.length * i, 0);
 		this.groundlayer_.appendChild(world[0][i]);
 	}
+	this.appendChild(this.groundlayer_);
 }
 
 goog.inherits(testbed.WorldView, lime.Scene);
